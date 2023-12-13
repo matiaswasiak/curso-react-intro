1138e54258663a87ff2402e12520f1930480525c
matiaswasiak <matiaswasiak@gmail.com>
2023-12-12 23:48:11 -0300

    Modal improvement

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -2,9 +2,17 @@ import { createPortal } from "react-dom";
 import "./Modal.css";
 
 function Modal({ children }) {
+  const modalRoot = document.getElementById("modal");
+  if (!modalRoot) {
+    console.error(
+      "Modal root element not found in the DOM. Please ensure an element with id 'modal' exists."
+    );
+    return null;
+  }
+
   return createPortal(
     <div className="ModalBackground">{children}</div>,
-    document.getElementById("modal")
+    modalRoot
   );
 }
 
